1b2af450206062249c47ba040049479a3b6fa733
Rohit Ware <rohitware99@gmail.com>
2026-01-26 17:38:52 +0530

    String character positions

---
+++ String/stringCharPosition.js
@@ -0,0 +1,10 @@
+var text = "HELLO WORLD";
+result = text.charAt(0);
+console.log(result);
+
+result1 = text.at(1);
+console.log(result1);
+
+
+resultCharcode = text.charCodeAt(0);
+console.log(resultCharcode);
