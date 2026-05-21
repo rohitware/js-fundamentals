f51eb55d29b3df349d427c945b35ce5a4f5c6b88
Rohit Ware <rohitware99@gmail.com>
2026-05-22 02:01:38 +0530

    Learn Nested Object in object literals

---
+++ Objects/nestedObject.js
@@ -0,0 +1,13 @@
+const student = {
+    name: "Rohit",
+
+    marks: {
+        marathi: 65,
+        maths: 78,
+        science: 88
+    }
+}
+console.log(student.name);
+console.log(student.marks.marathi);
+console.log(student.marks.maths);
+console.log(student.marks.science);
