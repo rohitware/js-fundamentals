3b1540d42682ad77146b9787a158e32c918d10e6
Rohit Ware <rohitware99@gmail.com>
2026-05-23 01:03:19 +0530

    Learn array of object

---
+++ Objects/arrayOfObjects.js
@@ -0,0 +1,11 @@
+const user = [
+    { name: "Rohit", age: 37 },
+    { name: "Amar", age: 39 },
+    { name: "Raghu", age: 35 },
+    { name: "Yash", age: 18 },
+];
+console.log(user[0]);
+console.log(user[1].name);
+console.log(user[2].age);
+console.log(user[3].name);
+
