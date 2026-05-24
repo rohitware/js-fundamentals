6ed8e590d325766657b9fbf38cae0a8f2381df26
Rohit Ware <rohitware99@gmail.com>
2026-05-25 02:50:19 +0530

    Implement shared prototype methods to optimize memory usage

---
+++ Objects/ConstructorFunctions/methodInConstructorFunction.js
@@ -0,0 +1,25 @@
+function Person(name, age) {
+    this.name = name;
+    this.age = age;
+}
+// method added to prototype
+
+Person.prototype.greet = function () {
+    console.log(`Hello i M ${this.name}`);
+}
+
+Person.prototype.showAge = function () {
+    console.log(`Hello im ${this.name} and my age is ${this.age}`);
+}
+
+const person1 = new Person("KK", 44);
+const person2 = new Person("shan", 45);
+
+console.log(person1);
+person1.greet();
+person1.showAge();
+
+console.log(person2);
+person2.greet();
+person2.showAge();
+
