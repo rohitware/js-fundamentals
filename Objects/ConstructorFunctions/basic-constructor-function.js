3065c7913e1a3caab879708c2506964194fc4b0c
Rohit Ware <rohitware99@gmail.com>
2026-05-25 02:42:21 +0530

    Learn object creation using constructor functions

---
+++ Objects/ConstructorFunctions/basic-constructor-function.js
@@ -0,0 +1,12 @@
+function User(name, age) {
+    this.name = name;
+    this.age = age;
+}
+
+// creating instant
+const user1 = new User("Kiran", 20);
+const user2 = new User("Samir", 22);
+
+console.log(user1);
+console.log(user2);
+
