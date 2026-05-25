5265c936ab344a95b955b45b4d2aed24349391bb
Rohit Ware <rohitware99@gmail.com>
2026-05-26 00:53:22 +0530

    add User class and instantiate user objects

---
+++ Objects/Classes/basic-class-syntax.js
@@ -0,0 +1,14 @@
+class User {
+    constructor(name, age) {
+        this.name = name;
+        this.age = age;
+    }
+}
+
+const user1 = new User("Ramesh", 32);
+const user2 = new User("kartik", 22);
+
+console.log(user1);
+console.log(user2);
+
+
