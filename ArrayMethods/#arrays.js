b28a446ff6fa571e1fdf1eda2df1d8699461172d
Rohit Ware <rohitware99@gmail.com>
2026-05-28 01:00:04 +0530

    array basic

---
+++ ArrayMethods/#arrays.js
@@ -0,0 +1,8 @@
+const fruits = ["apple", "banana", "mango"];
+
+console.log(fruits[0]);
+console.log(fruits[1]);
+console.log(fruits[2]);
+
+console.log(fruits.length);
+
---
+++ ArrayMethods/#creating-arrays.js
@@ -0,0 +1,25 @@
+// way 1 array literals (most common)
+
+const numbers = [1, 2, 3, 4, 5, 6];
+console.log(numbers);
+
+
+// way 2 mixed type
+
+const mixed = ["Rohit", 1, true, null];
+console.log(mixed);
+
+
+// way3 empty array
+
+const empty = [];
+console.log(empty);
+
+// Way 4 Array of objects
+
+const students = [
+    { name: "Rohit", age: 33 },
+    { name: "Rahul ", age: 22 },
+];
+console.log(students);
+
