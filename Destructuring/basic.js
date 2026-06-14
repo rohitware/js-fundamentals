bb9647ba3c0e78de7cf6f834bdac31cdc1447155
Rohit Ware <rohitware99@gmail.com>
2026-06-14 22:43:50 +0530

    Basic of object destructuring

---
+++ Destructuring/basic.js
@@ -0,0 +1,15 @@
+const person = {
+    name: "Rohit",
+    age: 33,
+    city: "Pune"
+};
+
+// Destructuring
+
+const { name, age, city } = person;
+
+console.log(name);
+console.log(age);
+console.log(city);
+
+
