518ac3f0f21f356d4cba9d413673c58e8fee6674
Rohit Ware <rohitware99@gmail.com>
2026-06-20 23:36:40 +0530

    example of a custom Promise with then/catch/finally

---
+++ Promise/fetchUserData.js
@@ -0,0 +1,27 @@
+function FetchUserData(userId) {
+    return new Promise((resolve, reject) => {
+        console.log("Fetching data");
+
+        setTimeout(() => {
+            if (userId === 1) {
+                resolve({ id: 1, name: "Rohit", city: "Pune" })
+            } else {
+                reject("User not found");
+            }
+        }, 2000);
+
+    });
+}
+
+FetchUserData(1)
+    .then(user => {
+        console.log(`Name: ${user.name}`);
+        console.log(`City: ${user.city}`);
+    })
+    .catch(error => {
+        console.log(`Error: ${error}`);
+    })
+    .finally(() => {
+        console.log(`Requsted completed`);
+
+    })
