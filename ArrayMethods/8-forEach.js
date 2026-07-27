2cd82e8c1e6f32dfa4eedb423ba1c1f35a56815e
Rohit Ware <rohitware99@gmail.com>
2026-07-28 00:31:24 +0530

    add forEach loop examples with standard and arrow functions

---
+++ ArrayMethods/8-forEach.js
@@ -0,0 +1,12 @@
+const fruits = ['apple', 'banana', 'cherry'];
+
+fruits.forEach(function (fruit) {
+    console.log(fruit);
+});
+
+
+
+// new example with arrow function
+const numbers = [1, 2, 3, 4, 5];
+
+numbers.forEach(number => console.log(number * 2)); 
