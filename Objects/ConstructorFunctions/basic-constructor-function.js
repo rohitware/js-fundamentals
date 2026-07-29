efc8cf6599c1c5ebc3f61bc2b58429bfc3355cc4
Rohit Ware <rohitware99@gmail.com>
2026-07-30 00:17:18 +0530

    Add constructor function examples: User and Product

--- Objects/ConstructorFunctions/basic-constructor-function.js
+++ Objects/ConstructorFunctions/basic-constructor-function.js
@@ -10,3 +10,19 @@ const user2 = new User("Samir", 22);
 console.log(user1);
 console.log(user2);
 
+// Another example: Product constructor
+function Product(title, price, stock) {
+    this.title = title;
+    this.price = price;
+    this.stock = stock;
+    this.getInfo = function () {
+        return `${this.title} costs $${this.price} (${this.stock} in stock)`;
+    };
+}
+
+const product1 = new Product("Laptop", 999, 5);
+const product2 = new Product("Mouse", 25, 50);
+
+console.log(product1.getInfo());
+console.log(product2.getInfo());
+
